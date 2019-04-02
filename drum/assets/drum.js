19bb610cea025e57c397776a527ee78cdc13d019
ewa1siwek <ew.siwek@gmail.com>
2019-04-02 23:31:37 +0200

    Blocking event on steady click

--- drum/assets/drum.js
+++ drum/assets/drum.js
@@ -1,7 +1,9 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  e.preventDefault();
   if(!audio) return;
+  
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
