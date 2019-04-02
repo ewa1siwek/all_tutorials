26b62cfce3ca65fb04e679698ece019d791782a9
ewa1siwek <ew.siwek@gmail.com>
2019-04-02 23:40:23 +0200

    Blocking clicking fixed, style of buttons fixed

--- drum/assets/drum.js
+++ drum/assets/drum.js
@@ -1,12 +1,11 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-  e.preventDefault();
   if(!audio) return;
-  
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
+  e.preventDefault();
 }
 
 function removeTransition(e) {
