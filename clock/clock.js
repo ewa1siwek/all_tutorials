8b92fa6627aa201b10f4f312cf5beb02dbba5a8d
ewa1siwek <ew.siwek@gmail.com>
2019-04-03 14:57:53 +0200

    Moved seconds & minutes hands

--- clock/clock.js
+++ clock/clock.js
@@ -6,3 +6,16 @@ secHand.style.background = 'white';
 minHand.style.background = 'red';
 hourHand.style.background = 'green';
 
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees  = ((seconds / 60) * 360) + 90;
+  secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000)
