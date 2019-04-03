60316f4c6871bdf359e8acd7bfffdf1694fd158d
ewa1siwek <ew.siwek@gmail.com>
2019-04-03 14:26:50 +0200

    Hands colored

---
+++ clock/clock.js
@@ -0,0 +1,8 @@
+const secHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+secHand.style.background = 'white';
+minHand.style.background = 'red';
+hourHand.style.background = 'green';
+
