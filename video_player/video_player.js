b1fc8105955ac71495a91f83c4a444e8da14b557
ewa1siwek <ew.siwek@gmail.com>
2019-04-10 22:28:20 +0200

    Handle range of volume

--- video_player/video_player.js
+++ video_player/video_player.js
@@ -29,11 +29,18 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  
+};
+
 // hook up the event listeners
 
-toggle.addEventListener('click', togglePlay)
-video.addEventListener('click', togglePlay)
-video.addEventListener('play', updateButton)
-video.addEventListener('pause', updateButton)
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 
-skipButtons.forEach(button => button.addEventListener('click', skip))
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
