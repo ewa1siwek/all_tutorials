8b65f43a3ac03749c80d5d3ba57199d9f8b5b8ab
ewa1siwek <ew.siwek@gmail.com>
2019-04-11 16:21:59 +0200

    Updated progress button during runtime

--- video_player/video_player.js
+++ video_player/video_player.js
@@ -2,7 +2,7 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progerss = player.querySelector('.progress');
-const progressBar = player.querySelector('progerss__filled');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
@@ -34,12 +34,19 @@ function handleRangeUpdate() {
   
 };
 
+function handleProgress() {
+  const percent = video.currentTime * 100 / video.duration;
+  progressBar.style.flexBasis = `${percent}%`;
+  console.log(percent)
+}
+
 // hook up the event listeners
 
 toggle.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress)
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
