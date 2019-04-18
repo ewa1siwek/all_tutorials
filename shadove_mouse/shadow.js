f9f5e3fae73f6ca8a73ce33a01d0d709a8202028
ewa1siwek <ew.siwek@gmail.com>
2019-04-19 00:59:35 +0200

    Refactoring

--- shadove_mouse/shadow.js
+++ shadove_mouse/shadow.js
@@ -21,7 +21,6 @@ function shadow(e) {
   ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
   ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
   ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
-
   `
 }
 
