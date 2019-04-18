2265a3b901b8ff0105a17880b2a180c9c416fe5b
ewa1siwek <ew.siwek@gmail.com>
2019-04-19 00:26:55 +0200

    Toggled position of child element on event

--- shadove_mouse/shadow.js
+++ shadove_mouse/shadow.js
@@ -2,10 +2,18 @@ const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 
 function shadow(e) {
-  const { offsetWidth: width, offsetHeight: height } = hero;
-  let { offsetX: x, offsetY: y } = e;
+  const width = hero.offsetWidth;
+  const height = hero.offsetHeight;
+  let x = e.offsetX;
+  let y = e.offsetY;
 
-  console.log(x, y)
+  if(this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+    console.log(x, y)
+  }
+
+  
 }
 
 hero.addEventListener('mousemove', shadow)
