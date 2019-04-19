2afba6e343f2c46674d6fcc8903156f330373898
ewa1siwek <ew.siwek@gmail.com>
2019-04-19 10:44:30 +0200

    Sorted array is displayed

--- sort/sort.js
+++ sort/sort.js
@@ -1,2 +1,10 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 
 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const bandsWindow = document.querySelector('#bands');
+
+const sortedBand = bands
+  .sort((a, b) => a < b ? -1 : 1)
+  .map(band => `<li>${band}</li>`)
+
+bandsWindow.innerHTML = `${sortedBand}`
