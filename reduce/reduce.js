850970fc516b8baf17819ff95df2cd7c943124b2
ewa1siwek <ew.siwek@gmail.com>
2019-04-19 12:30:17 +0200

    Figured out seconds

--- reduce/reduce.js
+++ reduce/reduce.js
@@ -2,5 +2,10 @@ const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 // const timeNodes = ([...document.querySelectorAll('[data-time]')]);
 
 const seconds = timeNodes
-  .map(node => node.dataset.time);
-console.log(seconds)
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':');
+    return mins * 60 + secs
+  });
+
+  console.log(seconds)
