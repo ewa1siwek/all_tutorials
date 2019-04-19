6def8189d1d3d328e83082b580327870dc0787fd
ewa1siwek <ew.siwek@gmail.com>
2019-04-19 10:57:57 +0200

    Sorted without the articles

--- sort/sort.js
+++ sort/sort.js
@@ -3,8 +3,12 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
 
 const bandsWindow = document.querySelector('#bands');
 
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim()
+}
+
 const sortedBand = bands
-  .sort((a, b) => a < b ? -1 : 1)
+  .sort((a, b) => strip(a) < strip(b) ? -1 : 1)
   .map(band => `<li>${band}</li>`)
 
 bandsWindow.innerHTML = `${sortedBand}`
