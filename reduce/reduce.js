5e6e9e96070588723ae8b57bda741dafb99c7ae0
ewa1siwek <ew.siwek@gmail.com>
2019-04-19 12:56:27 +0200

    Counted total hours, minutes and seconds left

--- reduce/reduce.js
+++ reduce/reduce.js
@@ -8,4 +8,12 @@ const seconds = timeNodes
     return (mins * 60) + secs
   }).reduce((total, vidSeconds) => total + vidSeconds);
 
-  console.log(seconds)
+  let secondsLeft = seconds;
+  const hours = Math.floor(secondsLeft / 3600);
+  secondsLeft = secondsLeft % 3600;
+  const minutes = Math.floor(secondsLeft / 60);
+  secondsLeft = secondsLeft % 60;
+
+
+
+  console.log(hours, minutes, secondsLeft)
