5f384a252fb2e2fddd1018961d4dbec8a4179cf1
ewa1siwek <ew.siwek@gmail.com>
2019-04-25 16:54:09 +0200

    Started count from first second

--- countdown_clock/countdown.js
+++ countdown_clock/countdown.js
@@ -1,18 +1,20 @@
-  let countdown;
+let countdown;
 
-  function timer(seconds) {
-    const now = Date.now();
-    const then = now + seconds * 1000;
-    contdown = setInterval(() => {
-      const secondsLeft = Math.round((then - Date.now()) / 1000);
-      
-      if (secondsLeft < 0) {
-        clearInterval(countdown)
-        return;
-      }
-      console.log(secondsLeft)
-    }, 1000);
-    
-  }
-  
-  
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft <= 0) {
+      clearInterval(countdown)
+      return;
+    }
+    displayTimeLeft(secondsLeft)
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  console.log(seconds)
+}
