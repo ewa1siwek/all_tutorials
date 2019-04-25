6f42c6b897d795ebb7164cb24ee8e1cb17b76bc4
ewa1siwek <ew.siwek@gmail.com>
2019-04-25 15:00:13 +0200

    Fixed randomHole function

--- walk_a_mole/mole.js
+++ walk_a_mole/mole.js
@@ -1,4 +1,24 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(hole)
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  console.log(time, hole)
+}
