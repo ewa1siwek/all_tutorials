56a515d9f8f7629936dd8c23de2c5fd8bb0e320f
ewa1siwek <ew.siwek@gmail.com>
2019-04-29 13:10:50 +0200

    Started counting from assumed number

--- countdown_clock/countdown.js
+++ countdown_clock/countdown.js
@@ -1,11 +1,21 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds)
   
   setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     //check if we should stop it
-    if (secondsLeft <= 0) return
-    console.log(secondsLeft)
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
   }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+  console.log(seconds)
 }
