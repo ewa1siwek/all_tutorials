435e7574e5170009681c887f3731a282b579a0d2
ewa1siwek <ew.siwek@gmail.com>
2019-04-29 12:49:38 +0200

    Created countdown function

--- countdown_clock/countdown.js
+++ countdown_clock/countdown.js
@@ -1,26 +1,5 @@
-let countdown;
-const timerDisplay = document.querySelector('.display__time-left')
-
 function timer(seconds) {
-  const now = Date.now();
-  const then = now + seconds * 1000;
-  displayTimeLeft(seconds);
-
-  countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    if (secondsLeft <= 0) {
-      clearInterval(countdown)
-      return;
-    }
-    displayTimeLeft(secondsLeft)
-  }, 1000);
-}
-
-function displayTimeLeft(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds > 9 ? remainderSeconds : '0' + remainderSeconds}`;
-  timerDisplay.textContent = display;
-  document.title = display;
-  console.log({minutes, remainderSeconds})
+  setInterval(function() {
+    seconds--;
+  }, 1000)
 }
