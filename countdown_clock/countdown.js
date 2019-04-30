4ba81e0e9cad2e7e9d8a929f34fd7776f0d89fb0
ewa1siwek <ew.siwek@gmail.com>
2019-04-30 16:15:21 +0200

    Added ended timer

--- countdown_clock/countdown.js
+++ countdown_clock/countdown.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  displayTimeLeft(seconds)
+  displayTimeLeft(seconds);
+  displayEndTime(then);
   
   setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -25,4 +27,11 @@ function displayTimeLeft(secondsLeft) {
       reminderSeconds : '0' + reminderSeconds}`;
   document.title = displayTime;
   timerDisplay.textContent = displayTime;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour}:${minutes}`;
 }
