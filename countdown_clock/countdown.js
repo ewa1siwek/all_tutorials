30ff2a874f89d1fe285cd982a5956ff8820aa403
ewa1siwek <ew.siwek@gmail.com>
2019-04-30 16:43:00 +0200

    Fixed bug with coundown vaiable, and muliple timers dispalying

--- countdown_clock/countdown.js
+++ countdown_clock/countdown.js
@@ -1,15 +1,18 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
-const buttons = document.querySelectorAll('[data-time]')
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  //clear any existing timers
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
   displayEndTime(then);
   
-  setInterval(() => {
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     //check if we should stop it
     if (secondsLeft < 0) {
