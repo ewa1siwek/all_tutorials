d9a729c7dbc578226f83cd1a07dfd81731f12ea9
ewa1siwek <ew.siwek@gmail.com>
2019-05-02 15:23:24 +0200

    Refactoring

--- viedeo_speed/video_speed.js
+++ viedeo_speed/video_speed.js
@@ -2,14 +2,18 @@ const speed = document.querySelector('.speed');
 const bar = speed.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
 
-speed.addEventListener('mousemove', function(e) {
+function handleMove(e) {
   const y = e.pageY - this.offsetTop;
   const percent = y / this.offsetHeight;
   const min = 0.4;
-  const ma = 4;
+  const max = 4;
   const height = Math.round(percent * 100) + '%';
 
-  bar.style.height = height;
+  const playbackRate = percent * (max - min) + min;
 
-  console.log(height);
-})
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + 'x';
+  video.playbackRate = playbackRate;
+}
+
+speed.addEventListener('mousemove', handleMove)
