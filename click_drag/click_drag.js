a2c1b40f07faa5b9562029c93faf8129f0338b33
ewa1siwek <ew.siwek@gmail.com>
2019-05-10 15:28:05 +0200

    Added first events

--- click_drag/click_drag.js
+++ click_drag/click_drag.js
@@ -2,3 +2,24 @@ const slider = document.querySelector('.items');
 let isDown = false;
 let stratX;
 let scrollLeft;
+
+slider.addEventListener('mousedown', () => {
+  isDown = true;
+  slider.classList.add('active');
+  stratX = e.pageX - slider.offsetLeft;
+})
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+})
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+})
+
+slider.addEventListener('mousemove', () => {
+  if(!isDown) return //stop the fn runnig
+  console.log(isDown);
+})
