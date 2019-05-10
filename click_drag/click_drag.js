a3fe85cb41b41424e7e3a09641e3a8c4cf2cfce7
ewa1siwek <ew.siwek@gmail.com>
2019-05-10 16:06:07 +0200

    Figured out the distance from first position

--- click_drag/click_drag.js
+++ click_drag/click_drag.js
@@ -7,6 +7,7 @@ slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
   startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
   console.log(startX)
   console.log(e.pageX)
 })
@@ -21,7 +22,11 @@ slider.addEventListener('mouseup', () => {
   slider.classList.remove('active');
 })
 
-slider.addEventListener('mousemove', () => {
-  if(!isDown) return //stop the fn runnig
-  console.log(isDown);
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return; //stop the fn runnig
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  console.log({x, startX})
+  const walk = x - startX;
+  console.log(walk)
 })
