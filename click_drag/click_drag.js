f26bd0aea0b8e58f56849827118952a8fc7b4f36
ewa1siwek <ew.siwek@gmail.com>
2019-05-10 16:13:59 +0200

    Added smotth effect

--- click_drag/click_drag.js
+++ click_drag/click_drag.js
@@ -26,7 +26,7 @@ slider.addEventListener('mousemove', (e) => {
   if(!isDown) return; //stop the fn runnig
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
-  const walk = x - startX;
+  const walk = (x - startX) * 3;
   slider.scrollLeft = scrollLeft - walk;
   console.log(walk)
 })
