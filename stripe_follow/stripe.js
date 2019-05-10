21c084dd5f7df53e7d39f56deae3a6f13473448b
ewa1siwek <ew.siwek@gmail.com>
2019-05-10 16:51:48 +0200

    Added sketch of events' functions

--- stripe_follow/stripe.js
+++ stripe_follow/stripe.js
@@ -1,3 +1,14 @@
 const triggers = document.querySelectorAll('.coll > li');
 const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
+
+function handleEnter() {
+
+}
+
+function handleLeave() {
+
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
