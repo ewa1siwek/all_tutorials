81522e3d52e6a36ce67385417c83623d08bb7a65
ewa1siwek <ew.siwek@gmail.com>
2019-05-20 13:20:07 +0200

    Useing stopPropagation() function

--- event/event.js
+++ event/event.js
@@ -1,7 +1,11 @@
 const dicvs = document.querySelectorAll('div');
 
 function logText(e) {
-  console.log(this.classList.value)
-}
+  console.log(this.classList.value);
+  e.stopPropagation();
+};
 
-dicvs.forEach(div => div.addEventListener('click', logText));
+
+dicvs.forEach(div => div.addEventListener('click', logText, {
+  capture: false
+}));
